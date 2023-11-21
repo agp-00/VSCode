4778547504948350f65027dd92e8f18360cc9596
agp-00 <agp00@iesemilidarder.com>
2023-11-21 20:21:13 +0100

    prueba

---
+++ fornite-end/README.md
@@ -0,0 +1,4 @@
+/*
+    Help: other version
+    https://www.youtube.com/watch?v=FJ9UtGfVkcI
+*/
--- prueba/prueba.java
+++
@@ -1,7 +0,0 @@
-package prueba;
-
-public class prueba {
-    public static void main(String [] args) {
-        System.out.println("hola");//skjdnkjs
-    }
-}
